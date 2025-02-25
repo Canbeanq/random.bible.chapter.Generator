c19fc5138d4289a1cb348649f013e8b85da606cf
Canbeanq <cookjesse1017@gmail.com>
2025-02-24 20:46:58 -0500

    version 0.1
    got the api working and installed requests

--- Random.bible.chapter.generator.py
+++ Random.bible.chapter.generator.py
@@ -1,1 +1,6 @@
-print('hello world')
+
+
+response = requests.get(
+    "https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/isaiah/chapters/1/verses/1.json")
+data = response.json()
+print(data["text"])
